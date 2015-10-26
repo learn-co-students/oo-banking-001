62322aa28690216f5233cb11d44c6ce74703f92b
Chris Aporta <christopher.aporta@gmail.com>
2015-10-25 20:13:29 -0400

    complete lab

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,35 @@
+require 'pry'
+
 class Transfer
-  # code here
+  attr_accessor :sender, :receiver, :status, :amount
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @amount = amount
+  end
+
+  def both_valid?
+    sender.valid? && receiver.valid? ? true : false
+  end
+
+  def execute_transaction
+    if !self.both_valid? || self.sender.balance < amount
+      self.status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    elsif !(self.status == "complete")
+      self.sender.balance -= self.amount
+      self.receiver.balance += self.amount
+      self.status = "complete"
+    end
+  end
+
+  def reverse_transfer
+    while self.status == "complete"
+      self.sender.balance += self.amount
+      self.receiver.balance -= self.amount
+      self.status = "reversed"
+    end
+  end
 end
